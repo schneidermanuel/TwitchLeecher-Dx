d3002cbe6f99775b3b02055d98a4af9dd1f08910
manuel <manuel@schneidermail.ch>
2024-05-16 11:19:10 +0200

    Just made the ui a little bit better, its still ugly

--- TwitchLeecher/GlobalAssemblyInfo.cs
+++ TwitchLeecher/GlobalAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("BrainySoftware")]
 [assembly: AssemblyCopyright("Copyright � 2023 Manuel Schneider")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("3.5.6")]
+[assembly: AssemblyFileVersion("3.5.8")]
 [assembly: ComVisible(true)]
