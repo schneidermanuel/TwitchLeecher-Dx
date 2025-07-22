c0dc880b054b823822e0b83da9f9cd8e92e1895e
mpeter50 <83356418+mpeter50@users.noreply.github.com>
2025-07-22 21:41:43 +0200

    move loading of scrolling position to base class

--- TwitchLeecher/TwitchLeecher.Gui/Views/DownloadsView.axaml.cs
+++ TwitchLeecher/TwitchLeecher.Gui/Views/DownloadsView.axaml.cs
@@ -1,45 +1,18 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using TwitchLeecher.Gui.Interfaces;
+﻿using Avalonia.Controls;
 
 namespace TwitchLeecher.Gui.Views
 {
-    public partial class DownloadsView : UserControl
+    public partial class DownloadsView : ScrollingView
     {
-        #region Fields
-
-        private INavigationState _state;
-
-        #endregion Fields
-
         #region Constructors
 
         public DownloadsView() {
             InitializeComponent();
-            
-            AttachedToVisualTree += ViewAttached;
-            scrollview.ScrollChanged += ScrollChanged;
+            SetupEventHandlers();
         }
 
         #endregion Constructors
 
-        #region EventHandlers
-
-        private void ScrollChanged(object sender, ScrollChangedEventArgs eventargs) {
-            if (_state != null) {
-                _state.ScrollPosition = scrollview.Offset.Y;
-            }
-        }
-
-        private void ViewAttached(object sender, EventArgs eventArgs) {
-            _state = DataContext as INavigationState;
-            
-            if (_state != null) {
-                // scrollview.Offset.X = _state.ScrollPosition;
-                scrollview.Offset = new Vector(0, _state.ScrollPosition);
-            }
-        }
-
-        #endregion EventHandlers
+        protected override ScrollViewer ScrollView => scrollview;
     }
 }
--- TwitchLeecher/TwitchLeecher.Gui/Views/SearchResultView.axaml.cs
+++ TwitchLeecher/TwitchLeecher.Gui/Views/SearchResultView.axaml.cs
@@ -1,41 +1,18 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using TwitchLeecher.Gui.Interfaces;
+﻿using Avalonia.Controls;
 
 namespace TwitchLeecher.Gui.Views
 {
-    public partial class SearchResultView : UserControl
+    public partial class SearchResultView : ScrollingView
     {
-        private INavigationState _state;
-        
         #region Constructors
 
         public SearchResultView() {
             InitializeComponent();
-
-            // old TL used the Loaded event, but now it results in a visible jump 
-            AttachedToVisualTree += ViewAttached;
-            scrollview.ScrollChanged += ScrollChanged;
+            SetupEventHandlers();
         }
 
         #endregion Constructors
 
-        #region EventHandlers
-
-        private void ScrollChanged(object sender, ScrollChangedEventArgs eventargs) {
-            if (_state != null) {
-                _state.ScrollPosition = scrollview.Offset.Y;
-            }
-        }
-
-        private void ViewAttached(object sender, EventArgs eventArgs) {
-            _state = DataContext as INavigationState;
-            
-            if (_state != null) {
-                scrollview.Offset = new Vector(0, _state.ScrollPosition);
-            }
-        }
-
-        #endregion EventHandlers
+        protected override ScrollViewer ScrollView => scrollview;
     }
 }
